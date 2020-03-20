e2207b2ece817f65ba6a1a61d6fb64308fbc6780
jamesnitz <jamesanitz2@gmail.com>
2020-03-20 15:50:27 -0500

    first commit

---
+++ DepartmentsEmployees/DepartmentsEmployees/Data/DepartmentRepository.cs
@@ -0,0 +1,121 @@
+﻿using DepartmentsEmployees.Models;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace DepartmentsEmployees.Data
+{
+    public class DepartmentRepository
+    {
+        public SqlConnection Connection
+        {
+            get
+            {
+                string _connectionString = "Data Source =localhost\\SQLEXPRESS; Initial Catalog=DepartmentsEmployees; Integrated Security=True";
+                return new SqlConnection(_connectionString);
+            }
+        }
+
+        public List<Department> GetAllDepartments()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    
+                    cmd.CommandText = @"
+                        Select Id, DeptName FROM Department";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Department> allDepartments = new List<Department>();
+
+                    while (reader.Read())
+                    {
+                        int idColumn = reader.GetOrdinal("Id");
+                        int idValue = reader.GetInt32(idColumn);
+
+                        int deptNameColumn = reader.GetOrdinal("DeptName");
+                        string departmentNameValue = reader.GetString(deptNameColumn);
+
+                        var department = new Department()
+                        {
+                            Id = idValue,
+                            DeptName = departmentNameValue
+                        };
+
+                        allDepartments.Add(department);
+                    }
+
+                    reader.Close();
+
+                    return allDepartments;
+                }
+            }
+        }
+        public void AddDepartment(Department department)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    // These SQL parameters are annoying. Why can't we use string interpolation?
+                    // ... sql injection attacks!!!
+                    cmd.CommandText = "INSERT INTO Department (DeptName) OUTPUT INSERTED.Id Values (@deptName)";
+                    cmd.Parameters.Add(new SqlParameter("@deptName", department.DeptName));
+                    int id = (int)cmd.ExecuteScalar();
+
+                    department.Id = id;
+                }
+            }
+
+            // when this method is finished we can look in the database and see the new department.
+        }
+
+        public Department GetDepartmentById(int departmentId)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        Select Id, DeptName FROM Department
+                         Where Id = @id";
+
+                    cmd.Parameters.Add(new SqlParameter("@id", departmentId));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        int idColumn = reader.GetOrdinal("Id");
+                        int idValue = reader.GetInt32(idColumn);
+
+                        int deptNameColumn = reader.GetOrdinal("DeptName");
+                        string departmentNameValue = reader.GetString(deptNameColumn);
+
+                        var department = new Department()
+                        {
+                            Id = idValue,
+                            DeptName = departmentNameValue
+                        };
+
+                        reader.Close();
+
+                        return department;
+                        
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+        }
+
+
+    }
+}
---
+++ DepartmentsEmployees/DepartmentsEmployees/Program.cs
@@ -0,0 +1,47 @@
+﻿using DepartmentsEmployees.Data;
+using DepartmentsEmployees.Models;
+using System;
+
+namespace DepartmentsEmployees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Setting a new instance of the employee repository and dept repo to a variable. Remeber to use the using statement at the top!
+            var repo = new EmployeeRepository();
+            var deptRepo = new DepartmentRepository();
+            //That both classes has method that gets all employees. Use it and store all them employees in a var.
+            var departments = deptRepo.GetAllDepartments();
+            var employees = repo.GetAllEmployees();
+
+            Department legal = new Department()
+            {
+                DeptName = "Legal"
+            };
+
+            //Use loops to write line
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName} is in {employee.Department.DeptName}!!");
+            };
+
+            //Use the method that gets a single employee by id and pass in an Id and set it to var.
+            var employeeWithId2 = repo.GetEmployeeById(2);
+            //Write it out
+            Console.WriteLine($"Employee with Id 2 is {employeeWithId2.FirstName} {employeeWithId2.LastName}");
+
+            foreach (var dept in departments)
+            {
+                Console.WriteLine($"{dept.DeptName}");
+            };
+
+            var deptWithId3 = deptRepo.GetDepartmentById(3);
+            Console.WriteLine($"Department numbah 3 is {deptWithId3.DeptName}");
+
+            //Adds a new department
+            //deptRepo.AddDepartment(legal);
+
+        }
+    }
+}
